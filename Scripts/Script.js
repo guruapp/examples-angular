4963d1d9e1adf2c77554dfca9cb0db20857ad110
Vadim L <netvideo@gmail.com>
2016-06-25 16:38:26 +0300

    Please enter the commit message for your changes. Lines starting 	modified:   Scripts/Script.js 	modified:   index.html

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,12 +1,12 @@
 var myApp = angular
     .module("myModule",[])
     .controller("myController", function ($scope){
-      $scope.employe =
-      {
-          firstName: "Vadim",
-          lastName: "Levoem",
-          gender: "male"
-      }
-        $scope.message ="Hello angular";
+      $scope.employes = [
+          {firstName:"Ben", lastName: "Lev", gender:"male",salary:55567},
+          {firstName:"Olya", lastName: "Kaza", gender:"female",salary:1155567},
+          {firstName:"Ben", lastName: "Lev", gender:"male",salary:23567},
+          {firstName:"Ben", lastName: "Lev", gender:"male",salary:345567}
+      ]
+
     });
 
