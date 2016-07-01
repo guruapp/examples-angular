a9cc871883e2279210dec4ce1d3b1b6894840db2
Vadim L <netvideo@gmail.com>
2016-07-01 18:51:04 +0300

    dsfsdf

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,11 +1,16 @@
 var myApp = angular
     .module("myModule",[])
     .controller("myController", function ($scope){
-      $scope.employes = [
-          {firstName:"Ben", lastName: "Lev", gender:"male",salary:55567},
-          {firstName:"Olya", lastName: "Kaza", gender:"female",salary:1155567},
-          {firstName:"Ben", lastName: "Lev", gender:"male",salary:23567},
-          {firstName:"Ben", lastName: "Lev", gender:"male",salary:345567}
+      $scope.countryes = [
+          {
+              name: "UK",
+              city: [{name: "new yourk"}, {name: "san francisko"}],
+          },
+          {
+              name:"Russia",
+              city:[{name:"Moscow"}]
+          }
+
       ]
 
     });
