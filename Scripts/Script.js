7627e794e27b4b6f87318f6b7553007e22c0fb43
Vadim L <netvideo@gmail.com>
2016-07-02 13:21:36 +0300

    likes

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,17 +1,21 @@
 var myApp = angular
     .module("myModule",[])
     .controller("myController", function ($scope){
-      $scope.countryes = [
-          {
-              name: "UK",
-              city: [{name: "new yourk"}, {name: "san francisko"}],
-          },
-          {
-              name:"Russia",
-              city:[{name:"Moscow"}]
-          }
+      $scope.technologies = [
+          {name:"C#", likes:0, dislikes:0 },
+          {name:"ASP.net", likes:0, dislikes:0 },
+          {name:"SQL server", likes:0, dislikes:0 },
+          {name:"angularJS", likes:0, dislikes:0 }
 
-      ]
+      ];
+        $scope.incrementLikes = function(technology)
+        {
+            technology.likes ++;
+        }
+        $scope.incrementDislikes = function(technology)
+        {
+            technology.likes --;
+        }
 
     });
 
